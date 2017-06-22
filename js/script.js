1cd1e60913c2fd649f006715c2de3343dd29db55
Brandon Hinds <brandonhinds@13gmail.com>
2017-06-22 12:04:03 -0500

    content

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-	$("#info").load("frags/skills.html");
+
+	$("#edu").click(function(){
+		$("#info").load("frags/education.html");
+	});
+
+	$("#skl").click(function(){
+		$("#info").load("frags/skills.html");
+	});
+
+	$("#exp").click(function(){
+		$("#info").load("frags/experience.html");
+	});
+
+	$("#hob").click(function(){
+		$("#info").load("frags/hobbies.html");
+	});
 });
