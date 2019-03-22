a4a2f36060db6afa0d046b8901fd43c6f8e2968a
Brandon Hinds <brandonhinds@13gmail.com>
2019-03-22 16:05:28 -0500

    New UI design

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
 $(document).ready(function(){
-
-	$("#edu").click(function(){
-		$("#info").load("frags/education.html");
-	});
-
-	$("#skl").click(function(){
-		$("#info").load("frags/skills.html");
-	});
-
-	$("#exp").click(function(){
-		$("#info").load("frags/experience.html");
-	});
-
-	$("#hob").click(function(){
-		$("#info").load("frags/hobbies.html");
-	});
+	//
 });
